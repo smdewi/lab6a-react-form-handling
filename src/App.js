93caab4cac09300f178aaf74f971b93764629a2b
smdewi <smdewi2020@gmail.com>
2022-12-06 20:36:38 +0800

    Base React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import SurveyForm from './SurveyForm';
+
+function App() {
+  return (
+    <div>
+      <SurveyForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SurveyForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class SurveyForm extends React.Component {
+    state = {
+        'name':'',
+        'colour':'',
+        'country':''
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                Form here
+            </React.Fragment>
+        );
+    }
+}
